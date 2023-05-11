a15a93c99861f3e859d646a9067256990d048e27
piyushpatel-atm <piyush29041999@gmail.com>
2023-05-11 16:06:38 +0530

    df

--- OnboardMeUI/src/app/app-routing.module.ts
+++ OnboardMeUI/src/app/app-routing.module.ts
@@ -3,19 +3,29 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { PreOnboardFormComponent } from './Components/pre-onboard-form/pre-onboard-form.component';
 import { HomePageComponent } from './Components/home-page/home-page.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { EmployeeHomeComponent } from './Components/employee-home/employee-home.component';
 
 const routes: Routes = [
   {
-    path:"Login",
-    component:LoginComponent
+    path: "", component: HomePageComponent
   },
+
   {
-    path:'PreForm',
-    component:PreOnboardFormComponent
+    path: "login", component: LoginComponent
   },
+
   {
-    path: "",component:HomePageComponent
+    path: "addemp", component: AddEmployeeComponent
   },
+
+  {
+    path: "emphome", component: EmployeeHomeComponent
+  },
+
+  {
+    path: "PreForm", component: PreOnboardFormComponent
+  }
 ];
 
 
