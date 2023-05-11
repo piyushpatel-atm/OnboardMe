7604a34d512aca01267c9d6a3631815fc7138e66
Yasir khan <yk1809786@gmail.com>
2023-05-11 16:00:17 +0530

    Emphome visibility check

--- OnboardMeUI/src/app/app-routing.module.ts
+++ OnboardMeUI/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoginComponent } from './Components/login/login.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { EmployeeHomeComponent } from './Components/employee-home/employee-home.component';
+import { PreOnboardFormComponent } from './Components/pre-onboard-form/pre-onboard-form.component';
 
 const routes: Routes = [
 {
   path: "",component:HomePageComponent
 },
+{
+  path:"login",component:LoginComponent
+},
+{
+  path:"addemp",component:AddEmployeeComponent
+},
+{
+  path:"emphome",component:EmployeeHomeComponent
+},
+{
+  path:"PreForm",component:PreOnboardFormComponent
+}
 ];
 
 @NgModule({
