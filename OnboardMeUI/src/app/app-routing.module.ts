944b9d3bd316af214350c5c3d0e5aae2c9726162
piyushpatel-atm <piyush29041999@gmail.com>
2023-05-11 15:40:34 +0530

    check the commit

--- OnboardMeUI/src/app/app-routing.module.ts
+++ OnboardMeUI/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Components/home-page/home-page.component';
+import { PreOnboardFormComponent } from './Components/pre-onboard-form/pre-onboard-form.component';
 
 const routes: Routes = [
 {
   path: "",component:HomePageComponent
 },
+{
+  path:"PreForm",component:PreOnboardFormComponent
+}
 ];
 
 @NgModule({
